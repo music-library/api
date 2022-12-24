e03ee267f51219f7fd2d8e8f5345d18b811a66e1
hmerritt <harry@mrrtt.me>
2022-12-24 16:10:07 +0000

    wip: indexer walk all files recursively

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -2,7 +2,8 @@ package indexer
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
+	"path/filepath"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -22,13 +23,29 @@ func (index *Index) Populate(path string) {
 	start := time.Now()
 	log.Debug("cache/populate start " + path)
 
-	files, err := ioutil.ReadDir(path)
+	err := filepath.Walk(path, func(itemPath string, info os.FileInfo, err error) error {
+
+		if err != nil {
+
+			fmt.Println(err)
+			return nil
+		}
+
+		// @TODO: Check extension
+		// filepath.Ext(path) == ".txt"
+
+		if !info.IsDir() {
+			fmt.Println(itemPath)
+
+			// @TODO: Add to index
+			// files = append(files, itemPath)
+		}
+
+		return nil
+	})
+
 	if err != nil {
 		log.Fatal("cache/populate failed ", err)
-	}
-
-	for _, f := range files {
-		fmt.Println(f.Name())
 	}
 
 	log.Debug(fmt.Sprintf("cache/populate end in %s %s", time.Since(start), path))
