80ba5faeabb759d162f3c4ac29f7aed6e4bffc96
hmerritt <harry@mrrtt.me>
2022-12-28 00:02:23 +0000

    wip: audio handler accept and stream range

--- api/track_audio_handler.go
+++ api/track_audio_handler.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"fmt"
+	"io"
 	"mime"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
@@ -35,7 +37,44 @@ func TrackAudioHandler(c *fiber.Ctx) error {
 		mimeType = "audio/flac"
 	}
 
-	c.Set(fiber.HeaderContentLength, fmt.Sprint(trackFileInfo.Size()))
+	totalSize := trackFileInfo.Size()
 	c.Set(fiber.HeaderContentType, mimeType)
+	c.Set(fiber.HeaderContentLength, fmt.Sprint(totalSize))
+
+	// Accept range requests
+	reqRange := c.Get(fiber.HeaderRange)
+	if reqRange != "" {
+		parts := strings.Split(strings.Replace(reqRange, "bytes=", "", 1), "-")
+		partialstart := parts[0]
+		partialend := parts[1]
+
+		start, err := strconv.ParseInt(partialstart, 10, 64)
+		if err != nil {
+			start = 0
+		}
+
+		end, err := strconv.ParseInt(partialend, 10, 64)
+		if err != nil || partialend != "" {
+			end = totalSize - 1
+		}
+
+		chunksize := end - start + 1
+
+		file, err := os.Open(track.Path)
+		if err != nil {
+			log.Error("http/track/" + trackId + "/audio track failed to play")
+		}
+
+		defer file.Close()
+		file.Seek(start, 0)
+		fileReader := io.LimitReader(file, totalSize-start)
+
+		c.Set(fiber.HeaderAcceptRanges, "bytes")
+		c.Set(fiber.HeaderContentRange, fmt.Sprintf("bytes %d-%d/%d", start, end, totalSize))
+		c.Set(fiber.HeaderContentLength, fmt.Sprint(chunksize))
+
+		return c.SendStream(fileReader)
+	}
+
 	return c.SendFile(track.Path, false)
 }
