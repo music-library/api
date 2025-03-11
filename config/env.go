92d0dc950acb5f6c0256c0c8d4559d539d9f6f49
hmerritt <harry@mrrtt.me>
2025-03-11 19:08:52 +0000

    fix: getEnv ignores empty values in favour of default value

--- config/env.go
+++ config/env.go
@@ -4,7 +4,7 @@ import "os"
 
 // Get environment variable or fallback to default
 func GetEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
+	if value, ok := os.LookupEnv(key); ok && value != "" {
 		return value
 	}
 	return fallback
